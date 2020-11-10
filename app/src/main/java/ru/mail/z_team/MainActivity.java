92d0c3e594a147f73d3e96a514f207a4f00711e1
n.ryzhkov <n.ryzhkov@corp.mail.ru>
2020-11-11 00:16:37 +0300

    executable now

--- app/src/main/java/ru/mail/z_team/MainActivity.java
+++ app/src/main/java/ru/mail/z_team/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        new NewUsedClass().sum(1, 6);
+        new NewUsedClass().sum(2, 6);
     }
 }
