5bf643d6841d9cd6d29dd16032d41566d25d4060
Исмагилова Дарья <armi.din1902@gmail.com>
2020-11-15 21:23:56 +0300

    fix names3

--- app/src/main/java/ru/mail/z_team/icon_fragments/friends/FriendsRepo.java
+++ app/src/main/java/ru/mail/z_team/icon_fragments/friends/FriendsRepo.java
@@ -7,14 +7,14 @@ import java.util.Collections;
 import java.util.List;
 
 public class FriendsRepo {
-    private final static MutableLiveData<List<Friend>> Friends = new MutableLiveData<>();
+    private final static MutableLiveData<List<Friend>> FRIENDS = new MutableLiveData<>();
 
     static {
-        Friends.setValue(Collections.<Friend>emptyList());
+        FRIENDS.setValue(Collections.<Friend>emptyList());
     }
 
     public LiveData<List<Friend>> getFriends(){
-        return Friends;
+        return FRIENDS;
     }
 
 }
