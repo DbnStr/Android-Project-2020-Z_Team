07be386f8b764c3cf9bdea4c640abde81e974e86
DbnStr <alexpich2002@yandex.ru>
2020-11-15 22:41:27 +0300

    connected storage

--- app/src/main/java/ru/mail/z_team/icon_fragments/ProfileFragment.java
+++ app/src/main/java/ru/mail/z_team/icon_fragments/ProfileFragment.java
@@ -1,5 +1,6 @@
 package ru.mail.z_team.icon_fragments;
 
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -10,10 +11,20 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+
 import ru.mail.z_team.R;
 
 public class ProfileFragment extends Fragment {
 
+    private static final String LOG_TAG = "ProfileFragment";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d("ProfileFragment", "OnCreate");
@@ -23,6 +34,25 @@ public class ProfileFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FirebaseStorage storage = FirebaseStorage.getInstance();
+        StorageReference storageReference = storage.getReference();
+        StorageReference path = storageReference.child("Users/1.txt");
+        final long ONE_MEGABYTE = 1024 * 1024;
+        path.getBytes(ONE_MEGABYTE).addOnSuccessListener(new OnSuccessListener<byte[]>() {
+            @Override
+            public void onSuccess(byte[] bytes) {
+                log(new String(bytes));
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                log("FAIL");
+            }
+        });
         return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    private void log(String message) {
+        Log.d(LOG_TAG, message);
     }
 }
