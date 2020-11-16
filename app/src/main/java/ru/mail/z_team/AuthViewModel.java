bf088c4754176458d9b6d5e3b1797820b4048217
Исмагилова Дарья <armi.din1902@gmail.com>
2020-11-16 23:47:50 +0300

    fix names

--- app/src/main/java/ru/mail/z_team/AuthViewModel.java
+++ app/src/main/java/ru/mail/z_team/AuthViewModel.java
@@ -21,17 +21,17 @@ import java.util.concurrent.Executor;
 public class AuthViewModel extends AndroidViewModel implements Executor {
 
     private static final String TAG = "AuthViewModel";
-    private final String NONE = getApplication().getResources().getString(R.string.NONE);
-    private final String ON_PROGRESS = getApplication().getResources().getString(R.string.ON_PROGRESS);
-    private final String ERROR = getApplication().getResources().getString(R.string.ERROR);
-    private final String SUCCESS = getApplication().getResources().getString(R.string.SUCCESS);
-    private final String FAILED = getApplication().getResources().getString(R.string.FAILED);
+    private final String none = getApplication().getResources().getString(R.string.NONE);
+    private final String onProgress = getApplication().getResources().getString(R.string.ON_PROGRESS);
+    private final String error = getApplication().getResources().getString(R.string.ERROR);
+    private final String success = getApplication().getResources().getString(R.string.SUCCESS);
+    private final String failed = getApplication().getResources().getString(R.string.FAILED);
     private final MediatorLiveData<Pair<String, String>> mAuthState = new MediatorLiveData<>();
     FirebaseAuth mAuth;
 
     public AuthViewModel(@NonNull Application application) {
         super(application);
-        mAuthState.setValue(new Pair<>(NONE, ""));
+        mAuthState.setValue(new Pair<>(none, ""));
     }
 
     public LiveData<Pair<String, String>> getProgress() {
@@ -41,7 +41,7 @@ public class AuthViewModel extends AndroidViewModel implements Executor {
     public void loginUser(String email, String password) {
         Log.d(TAG, "loginUser");
         mAuth = FirebaseAuth.getInstance();
-        mAuthState.postValue(new Pair<>(ON_PROGRESS, ""));
+        mAuthState.postValue(new Pair<>(onProgress, ""));
         mAuth.signInWithEmailAndPassword(email, password)
                 .addOnCompleteListener((Executor) this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -49,10 +49,10 @@ public class AuthViewModel extends AndroidViewModel implements Executor {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "signInWithEmail:success");
-                            mAuthState.postValue(new Pair<String, String>(SUCCESS, mAuth.getCurrentUser().getEmail()));
+                            mAuthState.postValue(new Pair<String, String>(success, mAuth.getCurrentUser().getEmail()));
                         } else {
                             // If sign in fails, display a message to the user.
-                            mAuthState.postValue(new Pair<>(FAILED, task.getException().toString()));
+                            mAuthState.postValue(new Pair<>(failed, task.getException().toString()));
                             Log.w(TAG, "signInWithEmail:failure", task.getException());
                         }
                     }
@@ -60,7 +60,7 @@ public class AuthViewModel extends AndroidViewModel implements Executor {
                 .addOnFailureListener(new OnFailureListener() {
                     @Override
                     public void onFailure(@NonNull Exception e) {
-                        mAuthState.postValue(new Pair<>(ERROR, e.getMessage()));
+                        mAuthState.postValue(new Pair<>(error, e.getMessage()));
                     }
                 });
     }
@@ -68,7 +68,7 @@ public class AuthViewModel extends AndroidViewModel implements Executor {
     public void registerUser(String email, String password) {
         Log.d(TAG, "registerUser");
         mAuth = FirebaseAuth.getInstance();
-        mAuthState.postValue(new Pair<>(ON_PROGRESS, ""));
+        mAuthState.postValue(new Pair<>(onProgress, ""));
         mAuth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                     @Override
@@ -76,11 +76,11 @@ public class AuthViewModel extends AndroidViewModel implements Executor {
                         if (task.isSuccessful()) {
                             // Sign in success, update UI with the signed-in user's information
                             Log.d(TAG, "createUserWithEmail:success");
-                            mAuthState.postValue(new Pair<>(SUCCESS, mAuth.getCurrentUser().getEmail()));
+                            mAuthState.postValue(new Pair<>(success, mAuth.getCurrentUser().getEmail()));
                         } else {
                             // If sign in fails, display a message to the user.
                             Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                            mAuthState.postValue(new Pair<>(FAILED, ""));
+                            mAuthState.postValue(new Pair<>(failed, ""));
                         }
                     }
                 })
@@ -88,7 +88,7 @@ public class AuthViewModel extends AndroidViewModel implements Executor {
                     @Override
                     public void onFailure(@NonNull Exception e) {
                         Log.e(TAG, e.getMessage());
-                        mAuthState.postValue(new Pair<>(ERROR, e.getMessage()));
+                        mAuthState.postValue(new Pair<>(error, e.getMessage()));
                     }
                 });
     }
