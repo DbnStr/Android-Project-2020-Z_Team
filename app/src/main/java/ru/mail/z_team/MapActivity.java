bf2287ca221b1b7262d9408c191c61bbc4bb0d79
Исмагилова Дарья <armi.din1902@gmail.com>
2020-11-20 17:46:01 +0300

    show simple map

---
+++ app/src/main/java/ru/mail/z_team/MapActivity.java
@@ -0,0 +1,80 @@
+package ru.mail.z_team;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private MapView mapView;
+    private MapboxMap mapboxMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map);
+
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
+        mapView = findViewById(R.id.map_view);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(this);
+
+    }
+
+    @Override
+    public void onMapReady(MapboxMap mapboxMap) {
+        this.mapboxMap = mapboxMap;
+        mapboxMap.setStyle(Style.MAPBOX_STREETS);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    @SuppressWarnings( {"MissingPermission"})
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+}
--- app/src/main/java/ru/mail/z_team/icon_fragments/go_out/GoOutFragment.java
+++ app/src/main/java/ru/mail/z_team/icon_fragments/go_out/GoOutFragment.java
@@ -1,28 +1,42 @@
 package ru.mail.z_team.icon_fragments.go_out;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import ru.mail.z_team.MapActivity;
 import ru.mail.z_team.R;
 
 public class GoOutFragment extends Fragment {
 
+    Button toMapBtn;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         Log.d("GoForWalk", "OnCreate");
+
         super.onCreate(savedInstanceState);
     }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_go_out, container, false);
+        View view = inflater.inflate(R.layout.fragment_go_out, container, false);
+        toMapBtn = view.findViewById(R.id.map_activity_btn);
+        toMapBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(requireActivity(), MapActivity.class));
+            }
+        });
+        return view;
     }
 }
