c697aa12aa5150ccb6777ed6ab18b24225df6386
Исмагилова Дарья <armi.din1902@gmail.com>
2020-11-28 22:24:05 +0300

    news recycler view (without api methods yet)

---
+++ app/src/main/java/ru/mail/z_team/icon_fragments/news/NewsViewHolder.java
@@ -0,0 +1,20 @@
+package ru.mail.z_team.icon_fragments.news;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ru.mail.z_team.R;
+
+public class NewsViewHolder extends RecyclerView.ViewHolder {
+    public final TextView newsTitle;
+    public final TextView newsDate;
+
+    public NewsViewHolder(@NonNull View itemView) {
+        super(itemView);
+        newsTitle = itemView.findViewById(R.id.news_item_title);
+        newsDate = itemView.findViewById(R.id.news_item_date);
+    }
+}
