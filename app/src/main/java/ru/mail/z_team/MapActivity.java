7a58343e3b42476564988145e3c111ac692f74b1
DbnStr <alexpich2002@yandex.ru>
2020-11-30 18:58:43 +0300

    change place to BMSTU, delete permanent points

--- app/src/main/java/ru/mail/z_team/MapActivity.java
+++ app/src/main/java/ru/mail/z_team/MapActivity.java
@@ -15,52 +15,39 @@ import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
-    private MapboxMap mapboxMap;
-    private Map<Integer, LatLng> points;
+
+    public MapActivity() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         Log.d("MapActivity", "onCreate");
         setContentView(R.layout.activity_map);
-        points = new HashMap<Integer, LatLng>();
 
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
 
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(MapboxMap mapboxMap) {
-                mapboxMap.setStyle(Style.MAPBOX_STREETS);
-                CameraPosition position = new CameraPosition.Builder()
-                        .target(new LatLng(55.75222, 37.61556))
-                        .zoom(14)
-                        .tilt(20)
-                        .build();
-                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 7000);
-                mapboxMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(55.75222, 37.61556))
-                        .title("first point"));
-                mapboxMap.addMarker(new MarkerOptions()
-                        .position(new LatLng(55.76, 37.62))
-                        .title("second point"));
-                mapboxMap.addOnMapClickListener((MapboxMap.OnMapClickListener) point -> {
-                    mapboxMap.addMarker(new MarkerOptions().position(point).title("new point"));
-                });
-            }
+        mapView.getMapAsync((OnMapReadyCallback) mapboxMap -> {
+            mapboxMap.setStyle(Style.MAPBOX_STREETS);
+            CameraPosition position = new CameraPosition.Builder()
+                    .target(new LatLng(55.765762, 37.685479))
+                    .zoom(14)
+                    .tilt(20)
+                    .build();
+            mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 7000);
+            mapboxMap.addOnMapClickListener((MapboxMap.OnMapClickListener) point -> mapboxMap.addMarker(new MarkerOptions()
+                    .position(point)
+                    .title("new point")));
         });
     }
 
     @Override
     public void onMapReady(MapboxMap mapboxMap) {
-        this.mapboxMap = mapboxMap;
         mapboxMap.setStyle(Style.MAPBOX_STREETS);
     }
 
