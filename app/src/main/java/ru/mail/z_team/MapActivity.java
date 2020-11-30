4ce49b3eedddda3e610900bc8f83b4e9cb949a2c
Rolso22 <ramagf@yandex.ru>
2020-11-30 18:42:58 +0300

    add marker

--- app/src/main/java/ru/mail/z_team/MapActivity.java
+++ app/src/main/java/ru/mail/z_team/MapActivity.java
@@ -1,31 +1,61 @@
 package ru.mail.z_team;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.annotations.MarkerOptions;
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.camera.CameraUpdateFactory;
 import com.mapbox.mapboxsdk.constants.Style;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private MapView mapView;
     private MapboxMap mapboxMap;
+    private Map<Integer, LatLng> points;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("MapActivity", "onCreate");
         setContentView(R.layout.activity_map);
+        points = new HashMap<Integer, LatLng>();
 
         Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
 
         mapView = findViewById(R.id.map_view);
         mapView.onCreate(savedInstanceState);
-        mapView.getMapAsync(this);
-
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS);
+                CameraPosition position = new CameraPosition.Builder()
+                        .target(new LatLng(55.75222, 37.61556))
+                        .zoom(14)
+                        .tilt(20)
+                        .build();
+                mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 7000);
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(55.75222, 37.61556))
+                        .title("first point"));
+                mapboxMap.addMarker(new MarkerOptions()
+                        .position(new LatLng(55.76, 37.62))
+                        .title("second point"));
+                mapboxMap.addOnMapClickListener((MapboxMap.OnMapClickListener) point -> {
+                    mapboxMap.addMarker(new MarkerOptions().position(point).title("new point"));
+                });
+            }
+        });
     }
 
     @Override
