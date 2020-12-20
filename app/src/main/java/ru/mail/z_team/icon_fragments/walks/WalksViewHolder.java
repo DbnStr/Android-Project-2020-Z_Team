b2c60d3de487df34c1a85610b43881a93bb9ec6b
Rolso22 <ramagf@yandex.ru>
2020-12-20 23:51:40 +0300

    continue design

--- app/src/main/java/ru/mail/z_team/icon_fragments/walks/WalksViewHolder.java
+++ app/src/main/java/ru/mail/z_team/icon_fragments/walks/WalksViewHolder.java
@@ -6,17 +6,19 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
+
 import ru.mail.z_team.R;
 
 public class WalksViewHolder extends RecyclerView.ViewHolder {
     public final TextView walkTitle;
     public final TextView walkDate;
-    public final View walkBody;
+    public final MaterialCardView walkBody;
 
     public WalksViewHolder(@NonNull View itemView) {
         super(itemView);
         walkTitle = itemView.findViewById(R.id.walk_item_title);
         walkDate = itemView.findViewById(R.id.walk_item_date);
-        walkBody = itemView.findViewById(R.id.walk_item);
+        walkBody = itemView.findViewById(R.id.walk_card);
     }
 }
